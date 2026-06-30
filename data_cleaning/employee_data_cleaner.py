156b910b6666bee7c8fe28be219519443bd87109
Yashaswini3103 <yashugowda1205@gmail.com>
2026-06-30 22:12:32 +0530

    Completed Data Cleaning project

--- data_cleaning/employee_data_cleaner.py
+++ data_cleaning/employee_data_cleaner.py
@@ -2,9 +2,18 @@ import pandas as pd
 
 df=pd.read_csv("employees_data.csv")
 
+print("Original Data:")
 print(df)
+
+print("\nMissing values:")
 print(df.isnull().sum())
-print(df.fillna(0))
-print(df.drop_duplicates())
+
+df = df.fillna(0)
+df=df.drop_duplicates()
 df=df.rename(columns={'Salary': 'Monthly_Salary'})
+
+print("\nCleaned Data:")
+print(df)
+
 df.to_csv("cleaned_employees.csv", index=False)
+print("\nFile saved successfully!")
